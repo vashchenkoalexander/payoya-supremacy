60873bec54204963c28ee43d47fa8ae03b0e6001
Alex <asdw3a@gmail.com>
2023-04-12 23:02:15 +0300

    init commit

---
+++ src/main/java/payoya/com/PayoyaBackendApplication.java
@@ -0,0 +1,13 @@
+package payoya.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayoyaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayoyaBackendApplication.class, args);
+	}
+
+}
